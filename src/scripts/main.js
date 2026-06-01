b9423373b2f0ad263ae4c77a155dc786dacb8402
Gabriel Santos <bielzingg92@gmail.com>
2026-06-01 08:34:41 -0300

    Implement questoes evaluation logic in main.js
    Add logic to evaluate 'questoes' and set 'questoesResultado' based on conditions.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,28 @@
+import { questoes } from "./questoes";
+let questoesResultado = 0
+
+if (questoes = true) {
+    questoesResultado++
+}
+
+if (questoesResultado<2) {
+    let texto = "Nem tente novamente lil bro 😭🥀🙏"
+} else if (questoesResultado>=2 && questoesResultado <4) {
+    let texto = "Você vai ser um eterno rookie!"
+} else if (questoesResultado>=4 && questoesResultado <6) {
+    let texto = "Você pode ser considerado MVP🏀⛹️"
+} else {
+    let texto = "Você é o GOAT🐐"
+}
+
+
+
+
+
+
+
+
+
+
 
 
